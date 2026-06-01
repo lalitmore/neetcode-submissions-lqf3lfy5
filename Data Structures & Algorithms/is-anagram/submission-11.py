a67919f633ddc9ddc2bde548f98cd01ef3918b39
Lalit More <77080372+lalitmore@users.noreply.github.com>
2026-06-01 06:46:17 -0400

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        map1 = {}
+        for x in s:
+            if x not in map1:
+                map1[x] = 1
+            else:
+                map1[x] = 1 + map1.get(x)
+        print(map1)
+        map2 = {}
+        for y in t:
+            if y not in map2:
+                map2[y] = 1
+            else:
+                map2[y] = 1 + map2.get(y)
+        if map1 == map2:
+            return True
+        return False
